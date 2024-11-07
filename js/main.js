6e2a9a835957c287e137e54ad959819866a88c51
VF <devphil@mail.ru>
2024-11-08 00:22:50 +0300

    Display images

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import Init from './InitData.js';
 import { createInfo } from './DataBuilder.js';
+import { displayPictures } from './DisplayPictures.js';
 
 function createData(value) {
   return Array.from({length: value}, createInfo);
 }
 
-createData(Init.QUANTITY);
+const dataBase = createData(Init.QUANTITY);
 // console.table(createData(Init.QUANTITY));
+displayPictures(dataBase);
