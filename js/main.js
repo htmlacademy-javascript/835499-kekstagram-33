6b64ac9e90bbfb726fe4d76891230c66226dbb92
VF <devphil@mail.ru>
2024-12-10 21:37:41 +0300

    Фильтры

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // import Init from './init-values.js';
 // import { createInfo } from './create-data.js';
-import { renderPictures } from './render-gallery.js';
+import { galleryHandler } from './render-gallery.js';
 import './upload-form-handler.js';
 import { getData } from './api.js';
 import { showErrorMessage } from './utils.js';
@@ -9,11 +9,11 @@ import { showErrorMessage } from './utils.js';
 //   return Array.from({length: value}, createInfo);
 // }
 // const dataBase = createData(Init.QUANTITY);
-// renderPictures(dataBase);
+// galleryHandler(dataBase);
 
 getData()
   .then((data) => {
-    renderPictures(data);
+    galleryHandler(data);
   })
   .catch(
     (err) => {
